287115e84d8c85ded7385c157dc23ab824a4062d
seanjeong3 <seanjeong3@gmail.com>
2019-02-07 21:51:17 -0800

    Update PyTorch Converter

--- models/deepnetwork/generate_torch_models.py
+++ models/deepnetwork/generate_torch_models.py
@@ -16,39 +16,39 @@ output_paths = {
     "VGG_16": os.path.join(root, "torch/VGG16.pmml"),
     "VGG_19": os.path.join(root,  "torch/VGG19.pmml"),
     "RESNET_50": os.path.join(root,  "torch/ResNet50.pmml"),
-    "INCEPTION_V3": os.path.join(root, "torch/InceptionV3.pmml"),
-    "DENSENET_121": os.path.join(root, "torch/DenseNet121.pmml"),
-    "DENSENET_169": os.path.join(root, "torch/DenseNet169.pmml"),
-    "DENSENET_201": os.path.join(root, "torch/DenseNet201.pmml")
+    # "INCEPTION_V3": os.path.join(root, "torch/InceptionV3.pmml"),
+    # "DENSENET_121": os.path.join(root, "torch/DenseNet121.pmml"),
+    # "DENSENET_169": os.path.join(root, "torch/DenseNet169.pmml"),
+    # "DENSENET_201": os.path.join(root, "torch/DenseNet201.pmml")
 }
 
 weight_urls = {
     "VGG_16": "https://s3.amazonaws.com/stanford-machine-learning/torch/pmml-models/VGG16.h5",
     "VGG_19": "https://s3.amazonaws.com/stanford-machine-learning/torch/pmml-models/VGG19.h5",
     "RESNET_50": "https://s3.amazonaws.com/stanford-machine-learning/torch/pmml-models/ResNet50.h5",
-    "DENSENET_121": "https://s3.amazonaws.com/stanford-machine-learning/pmml-models/torch/DenseNet121.h5",
-    "DENSENET_169": "https://s3.amazonaws.com/stanford-machine-learning/pmml-models/torch/DenseNet169.h5",
-    "DENSENET_201": "https://s3.amazonaws.com/stanford-machine-learning/pmml-models/torch/DenseNet201.h5"
+    # "DENSENET_121": "https://s3.amazonaws.com/stanford-machine-learning/pmml-models/torch/DenseNet121.h5",
+    # "DENSENET_169": "https://s3.amazonaws.com/stanford-machine-learning/pmml-models/torch/DenseNet169.h5",
+    # "DENSENET_201": "https://s3.amazonaws.com/stanford-machine-learning/pmml-models/torch/DenseNet201.h5"
 }
 
 torch_models = {
     "VGG_16": models.vgg16,
     "VGG_19": models.vgg19,
     "RESNET_50": models.resnet50,
-    "INCEPTION_V3": models.inception_v3,
-    "DENSENET_121": models.densenet121,
-    "DENSENET_169": models.densenet169,
-    "DENSENET_201": models.densenet201,
+    # "INCEPTION_V3": models.inception_v3,
+    # "DENSENET_121": models.densenet121,
+    # "DENSENET_169": models.densenet169,
+    # "DENSENET_201": models.densenet201,
 }
 
 descriptions = {
     "VGG_16": "VGG-16 ImageNet Model (from torchvision)",
     "VGG_19": "VGG-19 ImageNet Model (from torchvision)",
     "RESNET_50": "ResNet-50 ImageNet Model (from torchvision)",
-    "INCEPTION_V3": "Inception V3 ImageNet Model (from torchvision)",
-    "DENSENET_121": "DenseNet121 ImageNet Model (from torchvision)",
-    "DENSENET_169": "DenseNet169 ImageNet Model (from torchvision)",
-    "DENSENET_201": "DenseNet201 ImageNet Model (from torchvision)",
+    # "INCEPTION_V3": "Inception V3 ImageNet Model (from torchvision)",
+    # "DENSENET_121": "DenseNet121 ImageNet Model (from torchvision)",
+    # "DENSENET_169": "DenseNet169 ImageNet Model (from torchvision)",
+    # "DENSENET_201": "DenseNet201 ImageNet Model (from torchvision)",
 }
 
 
@@ -71,18 +71,18 @@ def load_imagenet_classes():
     return class_map
 
 
-def convert_torch_to_pmml(keras_model, output_path, weights_path, description, debug=True):
+def convert_torch_to_pmml(torch_model, output_path, weights_path, description, debug=True):
     """
     Convert a torch model to PMML
     """
     print("\nGenerating model: %s"%description)
     class_map = load_imagenet_classes()
-    pmml = convert(keras_model, class_map=class_map, description=description)
+    pmml = convert(torch_model, class_map=class_map, description=description)
     pmml.save_pmml(output_path, weights_path=weights_path, save_weights=False)
     if debug:
         print("Checking model: %s"%output_path)
         intermediate = DeepNetwork(filename=output_path)
-        old_config = keras_model.get_config()
+        old_config = torch_model.get_config()
         new_config = intermediate.get_keras_model().get_config()
         directory = os.path.dirname(output_path)
         dump_config(old_config, new_config, directory)
@@ -105,7 +105,7 @@ def build_models(models):
         description = descriptions[model_name]
         weights_path = weight_urls[model_name]
         output_path = output_paths[model_name]
-        convert_torch_to_pmml(model, output_path, weights_path, description)
+        convert_torch_to_pmml(model, output_path, weights_path, description, debug=False)
 
 
 
